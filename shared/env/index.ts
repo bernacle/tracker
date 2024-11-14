53624803bd5b6dfbeed517410600e1cff0d1e89e
Bruno Simplicio <bruno@notabene.id>
2024-11-14 09:44:41 -0400

    Initial commit

---
+++ shared/env/index.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    SERVER_PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().url(),
+  },
+  client: {},
+  shared: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    SERVER_PORT: process.env.SERVER_PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  emptyStringAsUndefined: true,
+})
