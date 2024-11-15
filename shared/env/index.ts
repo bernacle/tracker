fc41d2f05e7da91d605b04306dfdb6c6526752ff
Bruno Simplicio <bruno@notabene.id>
2024-11-15 10:18:59 -0400

    change on env

--- shared/env/index.ts
+++ shared/env/index.ts
@@ -5,15 +5,17 @@ export const env = createEnv({
   server: {
     SERVER_PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string().url(),
+    JWT_SECRET: z.string().default('secret'),
   },
   client: {},
   shared: {
-    NEXT_PUBLIC_API_URL: z.string().url(),
+    NEXT_PUBLIC_API_URL: z.string().url().default('http://localhost:9000'),
   },
   runtimeEnv: {
     SERVER_PORT: process.env.SERVER_PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
   emptyStringAsUndefined: true,
 })
